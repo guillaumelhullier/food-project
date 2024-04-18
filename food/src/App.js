503660d289da7991643ffb3da986b20ef75b31f5
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-04-18 18:08:29 +0200

    app

--- food/src/App.js
+++ food/src/App.js
@@ -1,21 +1,37 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+
+export default App;*/
+
+/*import React from "react";
+import { Route, Routes } from "react-router-dom";
+/*import Home from "./pages/Home";
+import Knowledges from "./pages/Knowledges";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";*/
+
+const App = () => {
+  return "Coucou tout le monde";
+  /*<div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/competences" element={<Knowledges />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>*/
+};
 
 export default App;
