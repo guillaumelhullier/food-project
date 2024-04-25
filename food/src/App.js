3c0c49326a775992c4acd6a331e9660635414e05
guillaumelhullier <89132296+guillaumelhullier@users.noreply.github.com>
2024-04-25 19:29:56 +0200

    app

--- food/src/App.js
+++ food/src/App.js
@@ -13,25 +13,14 @@ root.render(
 
 export default App;*/
 
-/*import React from "react";
-import { Route, Routes } from "react-router-dom";
-/*import Home from "./pages/Home";
-import Knowledges from "./pages/Knowledges";
-import Portfolio from "./pages/Portfolio";
-import Contact from "./pages/Contact";
-import NotFound from "./pages/NotFound";*/
+import React from "react";
 
 const App = () => {
-  return "Coucou tout le monde";
-  /*<div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/competences" element={<Knowledges />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>*/
+  return (
+    <div>
+      <h1>Je suis React</h1>
+    </div>
+  );
 };
 
 export default App;
